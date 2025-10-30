211a41516014a44ac5145ebbeab8e0ac243c7d3d
Sabio <gamester.sad@gmail.com>
2025-10-30 12:38:50 +0000

    Display current build hash

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  async function loadBuildVer() {
+    try {
+      const res = await fetch('/build.txt');
+      const txt = await res.text();
+
+      const cleanTxt = txt.replace(/[^\x20-\x7E]/g, '').trim();
+
+      const buildVerEl = document.getElementById('buildVer')
+      if (buildVerEl) {
+        buildVerEl.textContent = cleanTxt.slice(0, 6)
+      }
+    } catch {
+      console.error("Something went wrong whilst fetching build version!")
+    }
+  }
+
+  useEffect(() => {
+    loadBuildVer();
+  }, []);
+
   return (
     <div>
       <header style={{ display: "flex", flexDirection: "row", "margin": "2rem 10dvw 2rem 10dvw" }}>
@@ -18,7 +41,7 @@ export default function Home() {
         </div>
         <div style={{ display: "flex", flexDirection: "row", gap: "8px", marginLeft: "auto", alignItems: "center" }}>
           <p>
-            Build <span className="bold">(v0.1)</span>
+            Build <span id="buildVer"></span> <span className="bold small-font">(v0.1)</span>
           </p>
         </div>
       </header>
